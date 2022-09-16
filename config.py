6c19caa47fb5ad7f17ec6e55848b7fb2f9aec839
Ahmad Naufal Labiib Nabhaan <57089541+administrator2992@users.noreply.github.com>
2022-09-16 17:31:00 +0700

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 DEBUG = True
 
-SQLALCHEMY_DATABASE_URI = "mysql://KSSNcWgDPR:6kmXv6fBwc@remotemysql.com/KSSNcWgDPR"
+SQLALCHEMY_DATABASE_URI = "mysql://KSSNcWgDPR:6kmXv6fBwc@remotemysql.com:3306/KSSNcWgDPR"
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
